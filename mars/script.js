7adb4669fbeb49d8469a649d0185f9b8e95fddfc
geaseago <geoff.herbert@seattle.gov>
2016-03-16 14:47:27 -0700

    003 members hash working now.

--- mars/script.js
+++ mars/script.js
@@ -1,3 +1,52 @@
+var members  = [];
+var member   = {};
+var curr = "";
+var full = "";
+
+document.getElementById("pledge").onclick = function() {
+	member = {};
+	document.getElementById("stats").style.visibility = "visible";
+	member.name = document.getElementById("name").value; 
+	member.pwd  = document.getElementById("password").value;
+	member.eml  = document.getElementById("email").value;
+	member.amt  = document.getElementById("amount").value;
+	members.push(member);
+	
+	//alert(MakeMemberStats(members));
+	
+	document.getElementById("stats").innerHTML = MakeMemberStats(members);
+}
+
+function MakeMemberStats(m) {
+	curr = "";
+	full = "";
+	for (i=0; i<m.length; i++) {
+		//alert("txt for i value of " + i + " is--> " + m[i-1].name + " has pledged " + m[i-1].amt + ".<br>" );
+		curr = (m[i].name + " has pledged  $" + m[i].amt + ".<br>");
+		//alert("i=" + i + ". curr in loop ~~> " + curr);
+		full = full + curr;
+		//alert("i=" + i + ". full in loop ~~> " + full);
+		
+		/* SAVED SAMPLE THAT WORKED.
+		alert("i = " + i);
+		curr = "*";
+		full = (full + curr + ",");
+		alert("curr= " + curr);
+		alert("full= " + full);
+		*/
+	}
+	//alert("full outside of loop --> "+ full);
+	return full;
+}
+
+
+
+
+
+
+
+/* USEFUL SNIPPETS 
+
 var globe = true;
 
 function Flip(b,a) {                    //pneumonic for before, after 
@@ -13,19 +62,6 @@ function Modit() {
   document.getElementById("messages").innerHTML = "globe is currently " + globe;
 }
 
-
-
-
-
-
-
-
-
-
-
- 
-
-
 // save as an example 
 
 function BtnMessage(b) {   //b pneumonic for "button name"
@@ -37,5 +73,6 @@ function BtnMessage(b) {   //b pneumonic for "button name"
   }
 }
  
+*/ 
 
 
